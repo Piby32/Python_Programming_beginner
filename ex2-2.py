dddf27ae9cd73c1ebbda068ee686412d4c6a4920
Piby32 <piby1216@gmail.com>
2025-04-08 20:17:29 +0800

    Create ex2-2.py

---
+++ ex2-2.py
@@ -0,0 +1,9 @@
+name = input("enter your name: ")
+print(type(name))
+print("Hello, "+ name)
+
+age = input("enter your age: ")
+print(type(age))
+
+height = float(input("enter your height(m): "))
+print(type(height))
